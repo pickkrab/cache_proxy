5d54c388d472213dc92b5d43548c81b00e95d96f
AlexanderLukoyanov <pickkrab@gmail.com>
2016-08-20 16:38:14 +0300

    correct zip

--- src/main/java/ru/sbt/zip/Zip.java
+++ src/main/java/ru/sbt/zip/Zip.java
@@ -2,14 +2,25 @@ package ru.sbt.zip;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Zip {
     public static void zipFile(File file_name) {
         try {
             FileInputStream fileOutputStream = new FileInputStream(file_name);
-            //здесь должен быть алгоритм по записи байтов из файла в архив
+            ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(file_name.getName()));
+            ZipEntry pair = new ZipEntry(file_name.getName());
+            zipOutputStream.putNextEntry(pair);
+            byte[] list = new byte[1024];
+            int count;
+            while ((count = fileOutputStream.read(list)) > 0) {
+                zipOutputStream.write(list, 0, count);
+            }
             fileOutputStream.close();
+            zipOutputStream.close();
         } catch (IOException e) {
             throw new RuntimeException("Failed to compress");
         }
